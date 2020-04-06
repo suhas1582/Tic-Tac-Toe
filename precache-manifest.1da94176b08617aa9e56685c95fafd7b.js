e13f924031a09b3301654994b02f3fec5666c339
System Administrator <root@Suhass-MacBook-Pro.local>
2020-04-06 16:18:06 +0530

    Updates

---
+++ precache-manifest.1da94176b08617aa9e56685c95fafd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c37ee4b0b5dc51cf4803153b5780b4",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "ebd35224952ce35f2f68",
+    "url": "/Tic-Tac-Toe/static/css/2.8097fbaa.chunk.css"
+  },
+  {
+    "revision": "91252f0f8fc2caba3496",
+    "url": "/Tic-Tac-Toe/static/css/main.fec90cc1.chunk.css"
+  },
+  {
+    "revision": "ebd35224952ce35f2f68",
+    "url": "/Tic-Tac-Toe/static/js/2.25c16251.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Tic-Tac-Toe/static/js/2.25c16251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91252f0f8fc2caba3496",
+    "url": "/Tic-Tac-Toe/static/js/main.12a1b886.chunk.js"
+  },
+  {
+    "revision": "7fcc3f771d1c3a5d3d5b",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.d07bd4aa.js"
+  }
+]);
